13d4015773c34eb0a1d2987d8740cf840042d5f9
bottinolucas <lucasgabottino@gmail.com>
2025-02-04 23:16:04 -0300

    add: adicionando mais endpoints de usuario

--- api/infra/sqlalchemy/repositories/user.py
+++ api/infra/sqlalchemy/repositories/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, update, delete
 from sqlalchemy.orm import Session
 from api.schemas import schemas
 from api.infra.sqlalchemy.models import models
@@ -19,11 +19,25 @@ class UserRepository():
   
   def list_all(self):
     statement = select(models.User)
-    users = self.db.execute(statement).all()
+    users = self.db.execute(statement).scalars().all()
     return users 
   
-  def get(self):
-    ... 
+  def get_user_by_email(self, user_email: str):
+    return self.db.query(models.User).filter(models.User.email == user_email).first()
+
+  def update(self, email: str, user = schemas.User):
+    update_statement = update(models.User).where(
+      models.User.email == email
+    ).values(
+      password=user.password
+    )
+    self.db.execute(update_statement)
+    self.db.commit()
   
-  def remove(self):
-    ...
+  def remove(self, email: str):
+    delete_statement = delete(models.User).where(
+      models.User.email == email
+    )
+
+    self.db.execute(delete_statement)
+    self.db.commit()
