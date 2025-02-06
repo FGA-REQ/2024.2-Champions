f7ee2c334418c0f4be5bbbdd4bc2a39ac6840f79
arthurfonsecaa <arthurfonseca.vale@gmail.com>
2025-02-05 23:06:35 -0300

    add: tabela tarefa no banco de dados

--- api/infra/sqlalchemy/models/models.py
+++ api/infra/sqlalchemy/models/models.py
@@ -1,6 +1,6 @@
 # models.py
 
-from sqlalchemy import Column, String, ForeignKey, Table
+from sqlalchemy import Column, String, ForeignKey, Table, Text, Integer, Date
 from api.infra.sqlalchemy.config.db import Base
 from sqlalchemy.orm import relationship
 
@@ -25,6 +25,9 @@ class User(Base):
     password = Column(String)
     # Relacionamento muitos-para-muitos com Discipline
     disciplines = relationship('Discipline', secondary=user_discipline, back_populates='users')
+    # Relacionamento 1:1 com Calendar
+    tarefas = relationship('Tarefa', back_populates='user', cascade="all, delete")
+
 
 class Discipline(Base):
     __tablename__ = 'Discipline'
@@ -33,3 +36,15 @@ class Discipline(Base):
     discipline_name = Column(String)
     # Relacionamento muitos-para-muitos com User
     users = relationship('User', secondary=user_discipline, back_populates='disciplines')
+
+class Tarefa(Base):
+    __tablename__ = 'Tarefa'
+    
+    tarefa_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_email = Column(String, ForeignKey('User.email'))  # FK para User
+    data = Column(Date)          # Data da tarefa
+    assunto = Column(String)     # Assunto da tarefa
+
+    
+    # Relacionamento de volta para o usuário
+    user = relationship('User', back_populates='tarefas')
